28f5da533e1749cb244a444686f0820e3085e0d1
bopung <102723647+bopung@users.noreply.github.com>
2024-10-17 11:49:14 +0700

    change

--- features/step-definitions/login.steps.js
+++ features/step-definitions/login.steps.js
@@ -6,37 +6,37 @@ Given('I am on the login page of Paper.id', async () => {
 });
 
 When('I enter a valid email and password', async () => {
-    const emailField = await $('[data-cy="identifier"]');  // Updated selector
-    const nextButton = await $('[data-cy="submit"]');  // Updated selector for "Selanjutnya"
+    const emailField = await $('[data-cy="identifier"]'); 
+    const nextButton = await $('[data-cy="submit"]');  
     await emailField.setValue('kandidat@paper.id');
     await nextButton.click();
 
-    const passwordField = await $('[data-cy="password"]');  // Updated selector for password field
-    const loginButton = await $('[data-cy="submit"]');  // Updated selector for "Masuk"
+    const passwordField = await $('[data-cy="password"]');  
+    const loginButton = await $('[data-cy="submit"]'); 
     await passwordField.setValue('paper.id');
     await loginButton.click();
 });
 
 When('I enter an invalid email or password', async () => {
-    const emailField = await $('[data-cy="identifier"]');  // Updated selector
-    const nextButton = await $('[data-cy="submit"]');  // Updated selector for "Selanjutnya"
+    const emailField = await $('[data-cy="identifier"]');  
+    const nextButton = await $('[data-cy="submit"]'); 
     await emailField.setValue('invalid@paper.id');
     await nextButton.click();
 
-    const passwordField = await $('[data-cy="password"]');  // Updated selector for password field
-    const loginButton = await $('[data-cy="submit"]');  // Updated selector for "Masuk"
+    const passwordField = await $('[data-cy="password"]');  
+    const loginButton = await $('[data-cy="masuk"]');  
     await passwordField.setValue('invalidPassword');
     await loginButton.click();
 });
 
 Then('I should see the Paper.id dashboard', async () => {
     await browser.pause(3000);
-    const dashboardElement = await $('h1=Dashboard');  // Adjust the selector if needed
+    const dashboardElement = await $('h1=Dashboard');  
     assert(await dashboardElement.isDisplayed(), 'Dashboard was not displayed after login');
 });
 
 Then('I should see an error message', async () => {
     await browser.pause(3000);
-    const errorMessage = await $('p=Email atau kata sandi Anda salah');  // Adjust based on the actual error message on the page
+    const errorMessage = await $('p=Email atau kata sandi Anda salah');  
     assert(await errorMessage.isDisplayed(), 'Error message was not displayed for invalid login');
 });
